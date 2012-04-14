a16cf240ec0458cf730eb96b525957d577d865f4
Dominik Guzei <dominik_guzei@gmx.at>
2012-04-14 21:13:06 +0200

    sets up todomvc project in rails

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'jasminerice'
+  gem 'jasmine-stories'
+  gem 'guard-jasmine'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todomvc::Application.config.secret_token = 'fe7c60b5ce07bbd13d4e82a4ca6a32f94da8ee1df351fa8fe74e0f238197b65def2e36b6ed8752ddb35c4e6fd971a5844a33a2d1e684d92572c4cdd7f6dfb1da'
